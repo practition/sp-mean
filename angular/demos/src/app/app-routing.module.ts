4331385921c8458887ec523aa7a650f79c55af39
John Paxton <pax@speedingplanet.com>
2018-04-12 08:33:31 -0400

    Updated with a few new demos.

--- angular/demos/src/app/app-routing.module.ts
+++ angular/demos/src/app/app-routing.module.ts
@@ -1,10 +1,40 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OneRouteComponent } from './one-route/one-route.component';
+import { TwoRouteComponent } from './two-route/two-route.component';
+import { BasicObservableComponent } from './basic-observable/basic-observable.component';
+import { ObservableSuccessOrFailureComponent } from './observable-success-or-failure/observable-success-or-failure.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : 'one',
+    component : OneRouteComponent
+  },
+  {
+    path : 'two',
+    component : TwoRouteComponent
+  },
+  {
+    path: 'basic-observable',
+    component: BasicObservableComponent
+  },
+  {
+    path: 'observable-success-or-failure',
+    component: ObservableSuccessOrFailureComponent
+  },
+  {
+    path : '',
+    redirectTo : '/one',
+    pathMatch : 'full'
+  },
+  {
+    path : '**',
+    redirectTo : '/one'
+  }
+];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+@NgModule( {
+  imports : [ RouterModule.forRoot( routes ) ],
+  exports : [ RouterModule ]
+} )
+export class AppRoutingModule {}
--- angular/demos/src/app/app.module.ts
+++ angular/demos/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { OneRouteComponent } from './one-route/one-route.component';
+import { TwoRouteComponent } from './two-route/two-route.component';
+import { BasicObservableComponent } from './basic-observable/basic-observable.component';
+import { ObservableSuccessOrFailureComponent } from './observable-success-or-failure/observable-success-or-failure.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OneRouteComponent,
+    TwoRouteComponent,
+    BasicObservableComponent,
+    ObservableSuccessOrFailureComponent
   ],
   imports: [
     BrowserModule,
